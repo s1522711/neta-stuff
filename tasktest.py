9a27190941b0e50fd68a0a37507b8fcf2ce7e415
s1522711 <s1522711@gmail.com>
2023-01-04 17:43:18 +0200

    lockcock made changes

---
+++ tasktest.py
@@ -0,0 +1,24 @@
+import os
+
+
+numbers = []
+def sort():
+    numbers.sort()
+    print(*numbers, sep=", ")
+    print("The smallest number is:",numbers[0])
+    print("The biggest number is:",numbers[len(numbers)-1])
+
+numbers_entry_loop=0
+while numbers_entry_loop!=5:
+    try:
+        entered_number=float(input("Give me number "+str(numbers_entry_loop+1)+": "))
+    except ValueError:
+        print("Enter A Fucking Number!")
+    else:
+        if entered_number % 1 == 0:
+            numbers.append(int(entered_number))
+        else:
+            numbers.append(entered_number)
+        numbers_entry_loop+=1
+os.system('cls' if os.name == 'nt' else 'clear')
+sort()
