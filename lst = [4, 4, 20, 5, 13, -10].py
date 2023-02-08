458838f0db78301e7da7818752794c97778cf3a8
s1522711 <s1522711@gmail.com>
2023-02-08 17:15:11 +0200

    kjhkjh

---
+++ lst = [4, 4, 20, 5, 13, -10].py
@@ -0,0 +1,17 @@
+x = 3
+y = 5
+z = "x"
+
+if x != 3:
+    if z == x:
+        print("Hello")
+else:
+    print("Marhaba")
+
+if y - 4 == 1:
+    if z == x:
+        print("Shalom")
+    else:
+        print("Ola")
+else:
+    print("Bonjour")
